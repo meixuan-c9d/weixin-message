aacaaf16537f1fe93513c5a60e403ba72caa8b32
meixuan-c9d <mx@c9-d.com>
2020-11-20 14:50:07 +0800

    debug

--- middlewares/address-message-id.js
+++ middlewares/address-message-id.js
@@ -5,13 +5,18 @@ const checkIfLocalMessageIdFound = require('~/libs/check-if-local-message-id-fou
 const saveMessageId = require('~/libs/save-message-id')
 module.exports = promisifyAsync(async (request, response, next) => {
   const messageId = createMessageId(request.xmlBody)
+  debug.log(`
+    message id ${messageId}
+  `)
   const ifLocalMessageIdFound = checkIfLocalMessageIdFound(messageId)
   if (!ifLocalMessageIdFound) {
+    debug.log(`message id not found`)
     // message not found
     // save the entry
     saveMessageId(messageId)
     next()
   } else {
+    debug.log(`message id found`)
     // message found but yet processed
     // tell not to retry
     response.sendStatus(200)
