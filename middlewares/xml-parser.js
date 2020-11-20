b109f71b952ee3eabeb7cbb24e19c8f0f7849050
meixuan-c9d <mx@c9-d.com>
2020-11-20 12:29:35 +0800

    test parser

--- middlewares/xml-parser.js
+++ middlewares/xml-parser.js
@@ -5,12 +5,10 @@ const xmlParseString = promisify(xml2js.parseString)
 const promisifyAsync = require('../utils/promisify-async')
 
 module.exports = promisifyAsync(async(request, response, next) => {
-  debug.log('parser hit')
-  // const xmlBody = (await xmlParseString(request.body)).xml
-  // debug.log(`
-  //   xmlBody %O
-  // `, xmlBody)
-  // request.xmlBody = xmlBody
-
-  // next()
+  const xmlBody = (await xmlParseString(request.body)).xml
+  debug.log(`
+    xmlBody %O
+  `, xmlBody)
+  request.xmlBody = xmlBody  
+  response.sendStatus(200)
 })
