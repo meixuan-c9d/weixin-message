43b583c7ef6c7cc392d43fe90980a717574f8c71
meixuan-c9d <mx@c9-d.com>
2020-11-20 12:00:43 +0800

    debug parser

--- middlewares/xml-parser.js
+++ middlewares/xml-parser.js
@@ -2,9 +2,10 @@ const { promisify } = require('util')
 const debug = require('../configs/debug')
 const xml2js = require('xml2js')
 const xmlParseString = promisify(xml2js.parseString) 
-
 const promisifyAsync = require('../utils/promisify-async')
+
 module.exports = promisifyAsync(async(request, response, next) => {
+  debug.log('parser hit')
   const xmlBody = (await xmlParseString(request.body)).xml
   debug.log(`
     xmlBody %O
