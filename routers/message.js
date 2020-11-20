53b4c8f294e84f817e45c47986fe07f6c4666f8f
meixuan-c9d <mx@c9-d.com>
2020-11-20 11:44:54 +0800

    fix the duplicated variable name

--- routers/message.js
+++ routers/message.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 const middlewareVerification = require('../middlewares/verification')
 const middlewareXMLParser = require('../middlewares/xml-parser')
-const middlewareXMLParser = require('../middlewares/reply')
+const middlewareReply = require('../middlewares/reply')
 
 router
   .route('/')
