4f3574ad5db77d9cd3b9b9f16a966e2501cbcc4e
meixuan-c9d <mx@c9-d.com>
2020-12-01 20:39:37 +0800

    add text reply

---
+++ libs/replies/text.js
@@ -0,0 +1,38 @@
+const debug = require('~/configs/debug')
+const xmlBuilder = require('~/libs/xml-builder')
+const removeMessage = require('~/libs/remove-message.js')
+module.exports = (request, response, content) => {
+
+  console.log(content)
+  response.end()
+  return
+  
+  const xmlObject = request.xmlBody
+  const replyObject = {
+    ToUserName: xmlObject.FromUserName[0],
+    FromUserName: xmlObject.ToUserName[0],
+    CreateTime: Date.now() / 1000 | 0,
+    MsgType: 'news',
+    ArticleCount: 1,
+    Articles: {
+      item: [
+        {
+          Title: 'subscribe title',
+          Description: 'subscribe description',
+          PicUrl: 'n/a',
+          Url: 'https://www.baidu.com'
+        }
+      ]
+    }
+  }
+  const xml = xmlBuilder.buildObject(replyObject)
+  debug.log(`
+    replied XML
+    ${xml}
+  `)
+  
+  removeMessage(request.messageId)
+  response.type('text/xml')
+  response.send(xml)
+  return
+}
--- middlewares/reply.js
+++ middlewares/reply.js
@@ -1,6 +1,7 @@
 const debug = require('~/configs/debug')
 const wrap = require('~/utils/wrap')
 const replySubscribe = require('~/libs/replies/subscribe')
+const replyText = require('~/libs/replies/text')
 module.exports = wrap(async(request, response, next) => {
   debug.log(`
     replying...
@@ -8,6 +9,9 @@ module.exports = wrap(async(request, response, next) => {
   switch(request.xmlBody.Event[0]) {
     case 'subscribe':
       replySubscribe(request, response)
+      break
+    case 'text':
+      replyText(request, response, request.xmlBody.Content[0])
       break
     default:
       response.sendStatus(200)
